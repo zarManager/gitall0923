c2fa24c59102351c260aa632516e15dbc12e6983
zar <8023432@qq.com>
2022-09-23 17:39:35 +0800

    项目经理开发完项目提交

---
+++ git_001_javase/src/main/java/com/bjpowernode/Hello.java
@@ -0,0 +1,10 @@
+package com.bjpowernode;
+
+/**
+ *
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("这是javase的内容..........");
+    }
+}
---
+++ git_002_javaweb/src/main/java/com.bjpowernode/Manager.java
@@ -0,0 +1,10 @@
+package com.bjpowernode;
+
+/**
+ *
+ */
+public class Manager {
+    public static void main(String[] args) {
+        System.out.println("这是javaWeb的项目........");
+    }
+}
