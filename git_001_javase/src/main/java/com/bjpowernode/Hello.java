aaf22a6dc5b99ec5ec0f7fea3a1b5429ffe0a3be
zar <8023432@qq.com>
2022-09-24 09:06:19 +0800

    项目经理完成javaSE项目的Hello类的修改

--- git_001_javase/src/main/java/com/bjpowernode/Hello.java
+++ git_001_javase/src/main/java/com/bjpowernode/Hello.java
@@ -6,5 +6,6 @@ package com.bjpowernode;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("这是javase的内容..........");
+        System.out.println("项目经理更新后的内容");
     }
 }
