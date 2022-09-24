d4a0256df4626da8e6aabc360ffb26033356b802
zar <8023432@qq.com>
2022-09-24 09:48:24 +0800

    程序员克隆完成后在对项目修改和增加类

--- git_001_javase/src/main/java/com/bjpowernode/Hello.java
+++ git_001_javase/src/main/java/com/bjpowernode/Hello.java
@@ -7,5 +7,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("这是javase的内容..........");
         System.out.println("项目经理更新后的内容");
+        System.out.println("程序员克隆项目后项目经理新增的内容.......");
     }
 }
---
+++ git_002_javaweb/src/main/java/com.bjpowernode/One.java
@@ -0,0 +1,10 @@
+package com.bjpowernode;
+
+/**
+ *
+ */
+public class One {
+    public static void main(String[] args) {
+        System.out.println("程序员克隆项目后新增的类");
+    }
+}
